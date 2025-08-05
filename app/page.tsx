e95bdb91815f2fbfa867fa1f473bbafbf56bdf9e
reya <reya@lume.nu>
2025-08-05 16:10:56 +0700

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
-import { Link } from "next/link";
 import { Github } from "lucide-react";
+import Link from "next/link";
 
 export default function LandingPage() {
   return (
